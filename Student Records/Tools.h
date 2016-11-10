c01e128a22d8075e81b8a756a3d690ef395b39a7
hazelwin <hazelnutnguyen@gmail.com>
2016-11-10 12:16:19 -0800

    Update Tools.h

--- Student Records/Tools.h
+++ Student Records/Tools.h
@@ -8,16 +8,30 @@
 #define TOOLS_LOCK
 
 int getInt( std::string prompt );
+// precondtion: loop until user enters in a string
+// postcondtion: returns userinput
 
 std::string getString(std::string prompt);
 std::string getStringLine(std::string prompt);
 char getChar( std::string prompt );
+// precondtion: loop until user enters in a character
+// postcondtion: returns user input
 bool getBool( std::string prompt );
+// precondtion: calls getChar 
+// postcondtion: returns a bool statement depending on user input
 
 void flushInput(void);
+// precondtion: none
+// postcondtion: calls cin.ignore
+
 void pause( std::string prompt );
 
 void errorEmptyList(void);
+// precondtion: none
+// postcondtion: error input so dislay the list is empty
+
 void errorStudentNotFound(std::string identifier);
+// precondtion: none
+// postcondtion: display error message that student's record is not found
 
 #endif
