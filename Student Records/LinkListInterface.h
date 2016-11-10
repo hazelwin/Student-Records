9d62a7e6d47c693a8d63dbb0f78f05e85828b88d
hazelwin <hazelnutnguyen@gmail.com>
2016-11-10 11:42:12 -0800

    Update LinkListInterface.h

--- Student Records/LinkListInterface.h
+++ Student Records/LinkListInterface.h
@@ -4,6 +4,38 @@
 #include"StudentList.h"
 //#include"Tools.h"
 
+//void run(void);
+//precondtion: none
+//postcondition: test running is true to run loop
+
+// void displayCommands(void);
+//precondtion: none
+//postcondition: display all commands
+
+// void processCommands(StudentList* list, bool &running);
+// precondtion: none
+// postconidtion: cases to process the demands in displaycommands
+
+// void addStudent(StudentList* list);
+// preconidition: none
+// postcondition: id, student name, and courses linked
+
+// void deleteStudent(StudentList* list);
+// precondition: none
+// postcondtion: delete a student node
+
+// void addCourse(StudentList* list);
+// precondition: none
+// postcondtion: add a course
+
+// void deleteCourse(StudentList* list);
+// precondition: none
+// postcondtion: delete a course
+
+// void printStudentRecord(StudentList* list);
+// precondition:none
+// postcondtion: print entire student record
+
 namespace linklistinterface
 {
 	void run(void);
